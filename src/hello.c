fb2ed70e86ceb66a706f92e415fe80a82f82984b
David Richards <rawdod@gmail.com>
2025-12-03 03:23:44 -0500

    ok

---
+++ src/hello.c
@@ -0,0 +1,8 @@
+/* https://onlinegdb.com/BHcBwHpqj */
+/*C Hello World 26 */
+#include <stdio.h>
+int main() {
+  printf("EHLO Word %d\n", 0b10000 | 0b01000 | 0b00010);
+  return 0;
+}
+/*C Hi Project Genesis */
